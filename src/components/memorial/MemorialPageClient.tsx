6170b77ead72f78fd94f5117656424a5ef3229c8
eyurenalison <eeyuren1@gmail.com>
2026-01-30 17:09:19 +0100

    refactor: fixed CI errors

--- src/components/memorial/MemorialPageClient.tsx
+++ src/components/memorial/MemorialPageClient.tsx
@@ -33,7 +33,7 @@ interface MemorialWithTemplate extends Memorial {
     id: string;
     name: string | null;
     email: string;
-    accountDetails?: unknown[];
+    accountDetails?: unknown[] | null;
   };
   userTemplate:
     | (UserTemplate & {
